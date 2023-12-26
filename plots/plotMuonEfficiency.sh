71c6d7d3026e94f28b1846a24a5eed78715ac4d0
Jin Choi <choij@cern.ch>
2023-12-26 11:42:39 +0900

    update plots with tdr-style

---
+++ plots/plotMuonEfficiency.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+python plotEfficiency.py --key NUM_TopHNT_DEN_TrackerMuons --era 2016a
+python plotEfficiency.py --key NUM_TopHNT_DEN_TrackerMuons --era 2016b
+python plotEfficiency.py --key NUM_TopHNT_DEN_TrackerMuons --era 2017
+python plotEfficiency.py --key NUM_TopHNT_DEN_TrackerMuons --era 2018
+python plotEfficiency.py --key NUM_Mu17Leg1_DEN_TopHNT --era 2016a
+python plotEfficiency.py --key NUM_Mu17Leg1_DEN_TopHNT --era 2016b
+python plotEfficiency.py --key NUM_Mu17Leg1_DEN_TopHNT --era 2017
+python plotEfficiency.py --key NUM_Mu17Leg1_DEN_TopHNT --era 2018
+python plotEfficiency.py --key NUM_Mu8Leg2_DEN_TopHNT --era 2016a
+python plotEfficiency.py --key NUM_Mu8Leg2_DEN_TopHNT --era 2016b
+python plotEfficiency.py --key NUM_Mu8Leg2_DEN_TopHNT --era 2017
+python plotEfficiency.py --key NUM_Mu8Leg2_DEN_TopHNT --era 2018
